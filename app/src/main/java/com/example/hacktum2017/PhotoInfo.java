0b4337828bae87e38f2a568433c817f2e9dd969e
VitaNuova <vitanuova0512@gmail.com>
2017-11-18 21:32:44 +0100

    photo info edited

--- app/src/main/java/com/example/hacktum2017/PhotoInfo.java
+++ app/src/main/java/com/example/hacktum2017/PhotoInfo.java
@@ -35,7 +35,7 @@ public class PhotoInfo extends AppCompatActivity {
         description = intent.getStringExtra("description");
         latitude = intent.getDoubleExtra("latitude", 0.0);
         longitude = intent.getDoubleExtra("longitude", 0.0);
-        imageUrl = intent.getStringExtra("imageUrl");
+
 
         showDescription();
         findPicture();
@@ -48,27 +48,7 @@ public class PhotoInfo extends AppCompatActivity {
     }
 
     private void findPicture() {
-        try {
-            URL url = new URL(imageUrl);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            connection.connect();
-            int response = connection.getResponseCode();
-            InputStream is = connection.getInputStream();
-            BufferedInputStream bis = new BufferedInputStream(is);
-            Bitmap img = BitmapFactory.decodeStream(bis);
-            ImageView image = (ImageView) findViewById(R.id.image);
-            image.setImageBitmap(img);
-        } catch(MalformedURLException mex) {
-            String message = mex.getMessage();
-            Log.d("malformed url", message);
-        } catch(IOException ioex) {
-            String message = ioex.getMessage();
-            Log.d("ioexception", message);
-        } catch(Exception ex) {
-            String message = ex.getMessage();
-            Log.d("exception", message);
-        }
+
     }
 
     private void showDescription() {
@@ -77,11 +57,7 @@ public class PhotoInfo extends AppCompatActivity {
     }
 
     public void findHotelsButton(View v) {
-        try {
-            URL url = new URL(imageUrl);
-        } catch (Exception e) {
 
-        }
     }
 }
 
